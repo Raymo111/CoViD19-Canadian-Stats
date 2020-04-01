d200ea282f65cdc8e1cab6abeaf200c3a1f54aff
Raymond Li <hi@raymond.li>
2020-03-31 21:44:19 -0400

    It works yay

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class Iframe extends Component {
+	render() {
+		return <WebView source={{ uri: 'https://health-infobase.canada.ca/covid-19/iframe/map.html' }} />;
+		}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+	
+	return (
+		<Iframe />
+	);
+}
